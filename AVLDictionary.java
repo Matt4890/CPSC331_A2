a74314a2e23f456903bf927417eb472d58d6a4f8
Matt Allwright <matt.a.4890@gmail.com>
2018-10-29 13:24:41 -0600

    Added provided files

---
+++ AVLDictionary.java
@@ -0,0 +1,227 @@
+package cpsc331.assignment2;
+
+import java.util.NoSuchElementException;
+import cpsc331.collections.Dictionary;
+
+/**
+*
+* Provides an implementation of a Dictionary using an AVL tree.
+*
+*/
+
+// AVLDictionary Invariant:
+//
+// This AVL tree has nodes storing the ordered pairs in the
+// Dictionary being represented. Thus it is a binary search
+// tree such that all nodes have balance factors -1, 0, and 1.
+// Furthermore, if the AVL tree is not empty, then
+// - all the nodes in the left subtree include keys that are less
+//   than the key stored at the root,
+// - all the nodes in the right subtree include keys that are
+//   greater than the nodes at the root, and
+// - the left and right subtrees are also AVLDictionary's, so
+//   they also satisfy this invariant.
+
+public class AVLDictionary<K extends Comparable<K>, V>
+                            implements Dictionary<K, V> {
+                            
+  // Provides a node in this AVL tree
+  
+  class AVLNode {
+  
+    // Data Fields
+    
+    private K key;      // The key stored at the this node
+    private V value;    // The value stored at this node
+    private int height; // The height of the subtree with this node
+                        // as root
+    private AVLNode left;   // The left child of this node
+    private AVLNode right;  // The right child of this node
+    private AVLNode parent; // The parent of this node
+    
+    // Constructor; constructs an AVLNode with a given key and value
+    // whose left and right node and parent are initially null
+    
+    AVLNode(K k, V v) {
+    
+      key = k;
+      value = v;
+      height = 0;
+      left = null;
+      right = null;
+      parent = null;
+    
+    }
+    
+    // Returns the key stored at this node
+    
+    K key() {
+      return this.key;
+    }
+    
+    // Returns the value stored at this node
+    
+    V value() {
+      return this.value;
+    }
+    
+    // Returns the height of this node
+    
+    int height() {
+      return this.height;
+    }
+    
+    // Returns the left child of this node
+    
+    AVLNode left() {
+      return this.left;
+    }
+    
+    // Returns the right child of this node
+    
+    AVLNode right() {
+      return this.right;
+    }
+    
+    AVLNode parent() {
+      return this.parent;
+    }
+    
+    // Returns the balance factor of each node.
+    
+    int balanceFactor() {
+    
+      int leftHeight;  // Will be the height of the left child
+      int rightHeight; // Will be the height of the right child
+      
+      if (this.left == null) {
+        leftHeight = -1;
+      } else {
+        leftHeight = (this.left).height;
+      };
+      
+      if (this.right == null) {
+        rightHeight = -1;
+      } else {
+        rightHeight = (this.right).height;
+      };
+      
+      return leftHeight - rightHeight;
+    
+    }
+  
+  }
+  
+  // Data Fields
+  
+  private AVLNode root;
+  
+  /**
+  *
+  * Constructs an empty AVLDictionary<br><br>
+  *
+  * Precondition: None<br>
+  * Postcondition: An emptyAVLDictioary (satisfying the above
+  *                AVLDictionary Invariant) has been created.
+  *
+  */
+  
+  public AVLDictionary() {
+    root = null;
+  }
+  
+  // Returns a reference to the root of this AVLDictionary
+  
+  AVLNode root() {
+    return this.root;
+  }
+  
+  // Implements the "get" method provided by Dictionary
+  
+  public V get (K key) throws NoSuchElementException {
+  
+    return search(key, root);
+    
+  }
+  
+  // Implements the required "search" method; to be supplied by
+  // students
+  
+  private V search (K key, AVLNode x) throws NoSuchElementException {
+  
+    return null;     // This line must be replaced.
+  
+  }
+  
+  // Implements a left rotation at an input node; to be supplied
+  // by students
+  
+  private void rotateLeft (AVLNode x) {
+  
+  }
+  
+  // Implements a right rotation at an input node; to be supplied
+  // by students
+  
+  private void rotateRight (AVLNode x) {
+  
+  }
+  
+  // Implements the "set" method supplied by Dictionary
+  
+  public void set(K k, V v) {
+  
+    if (root == null) {
+    
+      root = new AVLNode(k, v);
+    
+    } else {
+    
+      change(k, v, root);
+    
+    }
+  
+  }
+  
+  // Implements the required "change" method; to be supplied
+  // by students
+  
+  private void change (K k, V v, AVLNode x) {
+  
+  }
+  
+  // Implements the "remove" method supplied by Dictionary
+  
+  public V remove (K k) throws NoSuchElementException {
+  
+    return deleteFromSubtree(k, root);
+    
+  }
+  
+  // Implements the required "deleteFromSubtree" method; to be
+  // supplied by students
+  
+  private V deleteFromSubtree(K k, AVLNode x)
+    throws NoSuchElementException {
+    
+    return null;          // This line must be replaced.
+    
+  }
+  
+  // Implements the required "deleteNode" method; to be supplied
+  // by students
+  
+  private void deleteNode (AVLNode x) {
+  
+  }
+  
+  // Implements the required "successor" method; to be supplied
+  // by students
+  
+  private AVLNode successor (AVLNode x) {
+  
+    return null;      // This line must be replaced.
+  
+  }
+                            
+}
---
+++ AVLUtilities.java
@@ -0,0 +1,225 @@
+package cpsc331.assignment2;
+
+import java.util.NoSuchElementException;
+import cpsc331.collections.Dictionary;
+import cpsc331.assignment2.AVLDictionary;
+import java.util.ArrayList;
+
+/**
+*
+* Provides utilities that can be used to test implementations
+* of AVLDictionary.
+*
+*/
+
+public class AVLUtilities<K extends Comparable<K>, V> {
+
+  /**
+  *
+  * Checks whether an input AVLDictionary is a valid AVL tree.<br>
+  *
+  * A second Boolean input is used to indicate whether
+  * diagnostic messages should be printed if it is determined
+  * that this is not, in fact, an AVL tree
+  *
+  */
+  
+  public boolean isAVLTree (AVLDictionary<K, V> D, boolean verbose) {
+  
+    boolean isBST = isBST(D, verbose);
+    boolean isBalanced = isBalanced(D, verbose);
+    return (isBST && isBalanced);
+  
+  }
+
+  /**
+  *
+  * Checks whether an input AVLDictionary is a valid
+  * binary search tree.<br>
+  *
+  * A second Boolean input is used to indicate whether
+  * diagonstic messages should be printed if it is determined
+  * that input tree is not, in fact, a binary search tree
+  *
+  */
+  
+  public boolean isBST (AVLDictionary<K, V> D, boolean verbose) {
+  
+    ArrayList<K> A = makeArray(D, verbose);
+    int length = A.size();
+    int i = 0;
+    while (i < length - 1) {
+      int result = (A.get(i)).compareTo(A.get(i+1));
+      if (result >= 0) {
+        return false;
+      };
+      i = i+1;
+    };
+    return true;
+  
+  }
+  
+  // Produces an ArrayList storing the keys in an input AVLDictionary.
+  //
+  // A second Boolean input is used as for isBST
+  //
+  // Based on the algorithm described in Tutorial Exercise #11
+  
+  private ArrayList<K> makeArray
+     (AVLDictionary<K, V> D, boolean verbose) {
+  
+    ArrayList<K> A = new ArrayList<K>();
+    if (D.root() != null) {
+      addToArray(D.root(), A, verbose);
+    };
+    return A;
+  
+  }
+  
+  
+  // Adds the entries of a subtree with an input node to an
+  // input ArrayList.
+  //
+  // A second Boolean input is used as for isBST
+  //
+  // Based on the algorithm described in Tutorial Exercise #11 -
+  // but also includes checks to ensure that the ArrayList
+  // being produced is sorted
+  
+  private void addToArray (AVLDictionary<K, V>.AVLNode x,
+                           ArrayList<K> A, boolean verbose) {
+                           
+    if (x.left() != null) {
+      addToArray(x.left(), A, verbose);
+      int length = A.size();
+      int leftResult = (x.key()).compareTo(A.get(length - 1));
+      if ((leftResult <= 0) && verbose) {
+        System.out.print((A.get(length-1)).toString());
+        System.out.print(" is in the left subtree of ");
+        System.out.println(x.key().toString());
+      };
+    };
+    
+    A.add(x.key());
+    int length = A.size();
+    
+    if (x.right() != null) {
+      addToArray(x.right(), A, verbose);
+      int rightResult = (x.key()).compareTo(A.get(length));
+      if ((rightResult >= 0) && verbose) {
+        System.out.print(A.get(length).toString());
+        System.out.print(" is in the right subtree of ");
+        System.out.println(x.key().toString());
+      };
+    };
+                         
+  }
+  
+  /**
+  *
+  * Checks whether all nodes in a (supposed) AVL tree are,
+  * indeed, balanced.<br>
+  *
+  * A second Boolean input is used to indicated whether
+  * diagonstic messages should be printed if it is determined
+  * that input tree is not, in fact, a binary search tree
+  *
+  */
+  
+  public boolean
+    isBalanced(AVLDictionary<K, V> D, boolean verbose) {
+  
+    if (D.root() == null) {
+      return true;
+    } else {
+      return isSubtreeBalanced(D.root(), verbose);
+    }
+    
+  }
+  
+  // Slow method to correctly check the height of a non-null node
+  
+  private int correctHeight(AVLDictionary<K, V>.AVLNode x) {
+  
+    int leftHeight = -1;
+    if (x.left() != null) {
+      leftHeight = correctHeight(x.left());
+    };
+    
+    int rightHeight = -1;
+    if (x.right() != null) {
+      rightHeight = correctHeight(x.right());
+    };
+    
+    return Math.max(leftHeight, rightHeight) + 1;
+  
+  }
+  
+  private int correctBalance(AVLDictionary<K, V>.AVLNode x) {
+  
+    int leftHeight = -1;
+    if (x.left() != null) {
+      leftHeight = correctHeight(x.left());
+    };
+    
+    int rightHeight = -1;
+    if (x.right() != null) {
+      rightHeight = correctHeight(x.right());
+    };
+    
+    return leftHeight - rightHeight;
+  
+  }
+  
+  // Slow method to correctly report the balance factor of a
+  // non-null node
+  
+  //
+  // Checks whether all nodes in the subtree of a given node
+  // are, indeed, balanced - and that heights of nodes are
+  // correctly defined
+  //
+  // A second Boolean input, verbose, is as for isBalanced
+  //
+  
+  private boolean isSubtreeBalanced
+     (AVLDictionary<K, V>.AVLNode x, boolean verbose) {
+                                                  
+    boolean leftBalanced = true;
+    if (x.left() != null) {
+      leftBalanced = isSubtreeBalanced(x.left(), verbose);
+    };
+    
+    boolean rightBalanced = true;
+    if (x.right() != null) {
+      rightBalanced = isSubtreeBalanced(x.right(), verbose);
+    };
+    
+    boolean correctHeight = true;
+    if (correctHeight(x) != x.height()) {
+      correctHeight = false;
+    };
+    if ((!correctHeight) && verbose) {
+      System.out.print("Height of node with key ");
+      System.out.print(x.key().toString());
+      System.out.println(" is incorrect.");
+    };
+    
+    int balanceFactor = correctBalance(x);
+    boolean nodeBalanced = ((balanceFactor == 1)
+                        || (balanceFactor == 0)
+                        || (balanceFactor == -1)
+                        || correctHeight);
+                        
+    if ((!nodeBalanced) && verbose) {
+      System.out.print("Node with key ");
+      System.out.print(x.key().toString());
+      System.out.print(" has balance factor ");
+      System.out.println(balanceFactor);
+    };
+    
+    return (leftBalanced && rightBalanced && nodeBalanced);
+                                                  
+  }
+
+}
