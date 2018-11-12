5c5745ae6cae60a9b66d40a1ede2e0b988f74421
Matt Allwright <matt.a.4890@gmail.com>
2018-11-12 14:28:15 -0700

    Formatted and documented. All tests still pass.

--- AVLDictionary.java
+++ AVLDictionary.java
@@ -157,7 +157,7 @@ public class AVLDictionary<K extends Comparable<K>, V> implements Dictionary<K, 
 	/**
 	 * Searches for a specified key in the tree and returns its value,
 	 * throwing an exception if the key is not in the tree.
-	 * @param  k                    The key to search for in the tree.
+	 * @param  k              	      The key to search for in the tree.
 	 * @param  x                      The node to start searching at.
 	 * @return                        The value of the node with the specified key.
 	 * @throws NoSuchElementException If the key is not in the tree,
@@ -166,22 +166,28 @@ public class AVLDictionary<K extends Comparable<K>, V> implements Dictionary<K, 
 
 		if (x == null) {
 
+			// Assertion: k is not in the tree.
 			throw new NoSuchElementException();
 
 		} else {
 
+			// Assertion: x is not null, and k may still be in the tree.
+
 			int result = k.compareTo(x.key);
-			
+
 			if (result == -1) {
 
+				// Assertion: If k is in the tree, it is in the left subtree of x.
 				return search(k, x.left());
 
 			} else if (result == 1) {
 
+				// Assertion: If k is in the tree, it is in the right subtree of x.
 				return search(k, x.right());
 
-			} else { // x.key().compareTo(key) == 0
+			} else { // result == 0
 
+				// Assertion: The key k is stored at x.
 				return x.value();
 
 			}
@@ -204,6 +210,7 @@ public class AVLDictionary<K extends Comparable<K>, V> implements Dictionary<K, 
 		// Handling Children
 		x.right		= z;
 		if (z != null){
+			// Assertion: y had a right child.
 			z.parent	= x;
 		}
 		y.left		= x;
@@ -212,21 +219,28 @@ public class AVLDictionary<K extends Comparable<K>, V> implements Dictionary<K, 
 		// Handling The Parent
 		if (x == root) {
 
-			root 		= y;
-			y.parent	= null;
+			// Assertion: x was root.
+			root = y;
 
 		} else {
 
-			if (x.key.compareTo(p.key) == -1) { // If x was a left child
+			// Assertion: x had a parent.
+			if (x.key.compareTo(p.key) == -1) {
+				// Assertion: x was a left child.
 				p.left	= y;
 			} else {
+				// Assertion: x was a right child.
 				p.right	= y;
 			}
 
-			y.parent = p;
-
 		}
 
+		y.parent = p;
+
+		// Update the heights
+		// QUESTION 17 DID NOT MENTION THIS CHANGE FROM A BST ROTATION.
+		// THIS IS BECAUSE THIS CHANGE WAS ADDED AFTER THE DEADLINE FOR THE WRITTEN PORTION.
+		// IT WAS ADDED TO HELP FIX THE FAILING TESTS. AN UPDATED DOCUMENT WILL BE SUBMITTED TO D2L.
 		x.updateHeight();
 		y.updateHeight();
 
@@ -246,6 +260,7 @@ public class AVLDictionary<K extends Comparable<K>, V> implements Dictionary<K, 
 		// Handling Children
 		x.left		= z;
 		if (z != null){
+			// Assertion: y had a right child.
 			z.parent	= x;
 		}
 		y.right		= x;
@@ -254,21 +269,28 @@ public class AVLDictionary<K extends Comparable<K>, V> implements Dictionary<K, 
 		// Handling The Parent
 		if (x == root) {
 
-			root 		= y;
-			y.parent	= null;
+			// Assertion: x was root.
+			root = y;
 
 		} else {
 
-			if (x.key.compareTo(p.key) == -1) { // If x was a left child
+			// Assertion: x had a parent.
+			if (x.key.compareTo(p.key) == -1) {
+				// Assertion: x was a left child.
 				p.left	= y;
 			} else {
+				// Assertion: x was a right child.
 				p.right	= y;
 			}
 
-			y.parent = p;
-
 		}
 
+		y.parent = p;
+
+		// Update the heights
+		// QUESTION 17 DID NOT MENTION THIS CHANGE FROM A BST ROTATION.
+		// THIS IS BECAUSE THIS CHANGE WAS ADDED AFTER THE DEADLINE FOR THE WRITTEN PORTION.
+		// IT WAS ADDED TO HELP FIX THE FAILING TESTS. AN UPDATED DOCUMENT WILL BE SUBMITTED TO D2L.
 		x.updateHeight();
 		y.updateHeight();
 
@@ -283,10 +305,16 @@ public class AVLDictionary<K extends Comparable<K>, V> implements Dictionary<K, 
 
 		if (root == null) {
 
+			// Assertion: The tree was empty.
+
+			// Make a new node at the root.
 			root = new AVLNode(k, v);
 
 		} else {
 
+			// Assertion: The tree was not empty.
+
+			// Either change the key to the new value, or insert a new node.
 			change(k, v, root);
 
 		}
@@ -307,38 +335,58 @@ public class AVLDictionary<K extends Comparable<K>, V> implements Dictionary<K, 
 
 		if (result == -1) {
 
+			// Assertion: If k is in the tree, it is in the left subtree of x.
 			if (x.left == null){
+				// Assertion: k is not in the tree.
+				// Insert k.
 				y			= new AVLNode(k, v);
 				x.left		= y;
 				y.parent	= x;
 			} else {
+				// Assertion: k may still be in the tree.
 				change(k, v, x.left);
 			}
 
 		} else if (result == 1) {
 
+			// Assertion: If k is in the tree, it is in the right subtree of x.
 			if (x.right == null){
+				// Assertion: k is not in the tree.
+				// Insert k.
 				y			= new AVLNode(k, v);
 				x.right		= y;
 				y.parent	= x;
 			} else {
+				// Assertion: k may still be in the tree.
 				change(k, v, x.right);
 			}
 
 		} else { // result == 0
 
+			// Assertion: The key k is stored at x.
 			x.value	= v;
 
 		}
 
 		// Go up the tree, starting at the inserted node,
 		// balancing any nodes that aren't balanced properly.
+		// Bound Function:
+		// 		f(d) = d + 1,
+		// 		where d = depth of y.
 		while (y != null) {
+
+			// Assertion: A node was inserted, and thus there may be a problem node with a bad balance factor.
 			y.updateHeight();
 			if (y.balanceFactor() == 2 || y.balanceFactor() == -2) {
+
+				// Assertion: Node y has a bad balance factor.
 				balanceNode(y);
+
 			}
+
+			// Move up to check the next node.
 			y = y.parent;
+
 		}
 
 	}
@@ -351,6 +399,7 @@ public class AVLDictionary<K extends Comparable<K>, V> implements Dictionary<K, 
 	 */
 	public V remove (K k) throws NoSuchElementException {
 
+		// Start searching for the node, starting at the root of the tree.
 		return deleteFromSubtree(k, root);
 
 	}
@@ -365,18 +414,36 @@ public class AVLDictionary<K extends Comparable<K>, V> implements Dictionary<K, 
 	private V deleteFromSubtree(K k, AVLNode x) throws NoSuchElementException {
 
 		if (x == null) {
+
+			// Assertion: k is not in the tree.
 			throw new NoSuchElementException();
+
 		} else {
+
+			// Assertion: x is not null, and k may still be in the tree.
+
 			int result = k.compareTo(x.key);
 			V v = x.value;
+
 			if (result == -1) {
+
+				// Assertion: If k is in the tree, it is in the left subtree of x.
 				return deleteFromSubtree(k, x.left);
+
 			} else if (result == 1) {
+
+				// Assertion: If k is in the tree, it is in the right subtree of x.
 				return deleteFromSubtree(k, x.right);
+
 			} else { // result == 0
+
+				// Assertion: The key k is stored at x.
 				deleteNode(x);
+
 			}
+
 			return v;
+
 		}
 
 	}
@@ -389,48 +456,102 @@ public class AVLDictionary<K extends Comparable<K>, V> implements Dictionary<K, 
 
 		AVLNode p = null;
 
-		if (x.left== null && x.right == null) { // x has no children
+		if (x.left== null && x.right == null) {
+
+			// Assertion: x has no children.
 
 			if (x == root) {
+
+				// Assertion: x was root.
 				root = null;
+
 			} else {
+
+				// Assertion: x had a parent.
+				// Clear all of x's references.
+
 				p = x.parent;
 				if (x.key.compareTo(p.key) == -1) {
+
+					// Assertion: x was a left child.
 					p.left = null;
+
 				} else {
+
+					// Assertion: x was a right child.
 					p.right = null;
+
 				}
+
 				x.parent = null;
+
 			}
 
-		} else if (x.left == null || x.right== null) { // x has 1 child
+		} else if (x.left == null || x.right== null) {
 
+			// Assertion: x has exactly 1 child.
+			// Promote that child to x's position.
+
+			// Find that child!
 			AVLNode c = null;
 			if (x.left == null) {
+
+				// Assertion: x's child is on the right.
 				c = x.right;
 				x.right = null;
+
 			} else {
+
+				// Assertion: x's child is on the left.
 				c = x.left;
 				x.left = null;
+
 			}
 
+			// Assertion: c now points to x's child.
+
+			// Promote c.
 			if (x == root) {
+
+				// Assertion: x was root.
 				c.parent = null;
 				root = c;
+
 			} else {
+
+				// Assertion: x had a parent.
+				// Clear all of x's references.
+
 				p = x.parent;
 				c.parent = p;
 				if (x.key.compareTo(p.key) == -1) {
+
+					// Assertion: x was a left child.
 					p.left = c;
+
 				} else {
+
+					// Assertion: x was a right child.
 					p.right = c;
+
 				}
+
 				x.parent = null;
+
 			}
 
-		} else { // x has 2 children
+		} else {
+
+			// Assertion: x has 2 children.
 
 			AVLNode s = successor(x);
+			// Assertion: s has a value greater than all nodes in the left
+			// subtree of x, but less than all nodes (excluding s itself)
+			// in the right subtree of x.
+			// This allows us to replace x with s without disturding the
+			// tree's BST property.
+
+			// Replace x's contents with s's contents, then delete s.
 			x.key = s.key;
 			x.value = s.value;
 			deleteNode(s);
@@ -439,16 +560,32 @@ public class AVLDictionary<K extends Comparable<K>, V> implements Dictionary<K, 
 
 		// Go up the tree, starting at the inserted node,
 		// balancing any nodes that aren't balanced properly.
+		// Bound Function:
+		// 		f(d) = d + 1,
+		// 		where d = depth of y.
 		while (p != null) {
+
+			// Assertion: There may be a problem node with a bad balance factor.
 			boolean equalCase = false;
 			p.updateHeight();
 			if (p.balanceFactor() == 2 || p.balanceFactor() == -2) {
+
+				// Assertion: Node p has a bad balance factor.
 				equalCase = balanceNode(p);
+
 			}
 			if (equalCase) {
+
+				// Assertion: A *-equal case was balanced.
+				// Thus, we know that all nodes above p have an acceptable balance
+				// factor, and we dont need to search anymore.
 				break;
+
 			}
+
+			// Move up to check the next node.
 			p = p.parent;
+
 		}
 
 	}
@@ -460,22 +597,33 @@ public class AVLDictionary<K extends Comparable<K>, V> implements Dictionary<K, 
 	 */
 	private AVLNode successor (AVLNode x) {
 
+		// Move right one node so that the successor is greater
+		// than all nodes stored in the left subtree of x.
 		x = x.right;
 
+		// Continually move left until there is no left child so
+		// that the successor is smaller than all other values in
+		// the right subtree of x.
+		// Bound Function:
+		// 		f(h) = h,
+		// 		where h = height of x.
 		while (x.left != null) {
 			x = x.left;
 		}
 
+		// Assertion: x is now a node greater than all nodes in the
+		// left subtree of the original x, but less than all remaining
+		// nodes in the right subtree of the original x.
 		return x;
 
 	}
 
 	/**
 	 * Balances a node after an insertion by performing the rotations
-	 * described in the assignment to solve the left-left, left-right,
-	 * right-left, and right-right cases.
+	 * described in the assignment to solve the left-left, left-equal,
+	 * left-right, right-left, right-equal, and right-right cases.
 	 * @param x The deepest node with a balance factor of -2 or 2.
-	 * @return 	True if a *-Equal case was balanced. False otherwise.
+	 * @return 	True if a *-equal case was balanced. False otherwise.
 	 */
 	private boolean balanceNode(AVLNode x) {
 
@@ -483,22 +631,32 @@ public class AVLDictionary<K extends Comparable<K>, V> implements Dictionary<K, 
 
 		if (x.balanceFactor() == 2) {
 
+			// Assertion: This is a left-* case.
+
 			result = x.left.balanceFactor();
 			if (result == -1) {
+				// Assertion: This is a left-right case.
 				rotateLeft(x.left);
 			}
 			rotateRight(x);
 
 		} else if (x.balanceFactor() == -2) {
 
+			// Assertion: This is a right-* case.
+
 			result = x.right.balanceFactor();
 			if (result == 1) {
+				// Assertion: This is a right-left case.
 				rotateRight(x.right);
 			}
 			rotateLeft(x);
 
 		}
 
+		// Assertion: All nodes in the subtree with the new parent
+		// of x as root have a balance factor within {-1, 0, 1}.
+
+		// Assertion: If result == 0, then a *-equal case was balanced.
 		return result == 0;
 
 	}
